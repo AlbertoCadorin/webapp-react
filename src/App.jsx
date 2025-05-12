3fc6b8a98c0d3ead1803e21f699e9658e5fdf360
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-05-13 01:02:28 +0200

    feat: implement routing with react-router-dom in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
 
 
   return (
-    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/movies" element={<h1>Elenco film</h1>} />
+        <Route path="/movies/:id" element={<h1>Dettaglio film</h1>} />
+        <Route path="/contact" element={<h1>Contatti</h1>} />
+        <Route path="/about" element={<h1>Chi siamo</h1>} />
+        <Route path="*" element={<h1>404</h1>} />
 
-    </>
+      </Routes>
+    </BrowserRouter>
   )
 }
 

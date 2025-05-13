cc378787fcc6f12224571e9eac69e7f6f870ad09
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-05-13 02:46:49 +0200

    feat: update MoviesCard component to accept movie data as props and enhance MoviesPage with movie fetching logic

--- src/componets/MoviesCard.jsx
+++ src/componets/MoviesCard.jsx
@@ -1,15 +1,20 @@
-const MoviesCard = () => {
+import { Link } from "react-router-dom";
+
+const MoviesCard = ({ data }) => {
+
+    const { id, title, abstract, release_year } = data;
     return (
-        <div className="col-3 col-md-4">
-            <div className="card">
-                <img src="" alt="Film" />
-                <div className="card-body">
-                    <h5 className="card-title">Film 1</h5>
-                    <p className="card-text">Descrizione del film 1</p>
-                    <a href="/movies/1" className="btn btn-primary">Dettagli</a>
-                </div>
+
+        <div className="card">
+            <img src="" alt="Film" />
+            <div className="card-body">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">Release year: {release_year}</p>
+                <p className="card-text">{abstract}</p>
+                <Link to={`/movies/${id}`} className="btn btn-primary">Dettagli</Link>
             </div>
         </div>
+
     )
 }
 
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,13 +1,40 @@
+import { useEffect, useState } from "react";
 import MoviesCard from "../componets/MoviesCard";
+import axios from "axios";
+
 const MoviesPage = () => {
+
+    const [movies, setMovies] = useState([])
+
+    function getMovies() {
+        axios.get("http://localhost:3000/movies")
+            .then(res => {
+                console.log(res.data);
+                setMovies(res.data);
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    }
+
+    useEffect(getMovies, [])
+
+
     return (
         <>
             <h1 className="mb-2">Film</h1>
             <section>
+
                 <h2 className="mb-2">Lista Film</h2>
-                <div className="row">
-                    <MoviesCard />
+
+                <div className="row gy-3">
+                    {movies.length ? movies.map(movie => (
+                        <div className="col-3 col-md-4" key={movie.id}>
+                            <MoviesCard data={movie} />
+                        </div>
+                    )) : <div>Non ci sono film</div>}
                 </div>
+
             </section>
         </>
     );
